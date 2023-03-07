1073bd743ff9afb010d8a0abb8c5fc84e38e3b1d
pblprz <70571411+pblprz@users.noreply.github.com>
2023-03-07 18:37:24 +0100

    Fix(CCBHash): Use stable version

--- ccbhash/ccbhash.py
+++ ccbhash/ccbhash.py
@@ -2,7 +2,7 @@ import os
 import json
 import r2pipe
 from hashlib import blake2b
-from statistics import mean, StatisticsError
+from statistics import mean
 
 weights = { 'opcodes': 0.25,
             'cfg': 0.16,
@@ -19,16 +19,6 @@ weights = { 'opcodes': 0.25,
             'name': 0.01 }
 
 def _get_variables(finf):
-    '''
-    Get variables (args and vars) from Radare assembly function.
-
-    Args:
-    - finf = r2.cmdj("afij")
-
-    Returns:
-    - variables ({ 'args': str, 'vars': str }): Args and vars using str format
-    '''
-
     args = {}
     vars = {}
 
@@ -42,8 +32,7 @@ def _get_variables(finf):
                     if var['type'] not in vars: vars[var['type']] = 1
                     else: vars[var['type']] += 1 
     except:
-        # It should never pass this way.
-        raise Exception(f'Error to get variables')
+        pass
     
     args = dict(sorted(args.items()))
     vars = dict(sorted(vars.items()))
@@ -58,17 +47,6 @@ def _get_variables(finf):
     return {'args': args_line, 'vars': vars_line}
 
 def _get_callgraph(function_name, global_callgraph):
-    '''
-    Get CallGraph from Radare assembly function.
-
-    Args:
-    - function_name (str): function name
-    - global_callgraph = r2.cmdj("agCj")
-
-    Returns:
-    - callgraph (str): Callgraph using str format (similar to Machoke)
-    '''
-
     callgraph: dict[str, list[str]] = {}
     functions_id: dict[str, str] = {}
     functions_left: list[str] = []
@@ -76,15 +54,15 @@ def _get_callgraph(function_name, global_callgraph):
     
     functions_left.append(function_name)
     while len(functions_left) > 0:
-        current_function = functions_left.pop(0)
-        functions_id[current_function] = str(id_function)
+        actual_function = functions_left.pop(0)
+        functions_id[actual_function] = str(id_function)
         id_function += 1
         for function in global_callgraph:
-            if function['name'] == current_function:
-                if current_function not in callgraph: callgraph[current_function] = []
+            if function['name'] == actual_function:
+                if actual_function not in callgraph: callgraph[actual_function] = []
                 for f in function['imports']:
-                    if f not in callgraph[current_function]: 
-                        callgraph[current_function].append(f)
+                    if f not in callgraph[actual_function]: 
+                        callgraph[actual_function].append(f)
                         if f not in functions_left: functions_left.append(f)
                 break
 
@@ -95,24 +73,12 @@ def _get_callgraph(function_name, global_callgraph):
             try:
                 line += functions_id[output] + ','
             except:
-                # It fails because output is not in the function. It happens because of Radare.
-                # It can be ignored although it should be improved.
-                print(f'[!!] Failed to get callgraph. From node {name} to node {output}')
+                pass
         line = (line[:-1] if len(outputs) > 0 else line) + ";"
 
     return line
 
 def _get_cfg(fcode):
-    '''
-    Get Control Flow Graph from Radare assembly function.
-
-    Args:
-    - fcode = r2.cmdj("agj")
-
-    Returns:
-    - cfg (str): Control Flow Graph using str format (similar to Machoke)
-    '''
-
     blocks_outputs: dict[str, list[str]] = {}
     blocks_id: dict[str, str] = {}
     id_block = 0
@@ -132,24 +98,12 @@ def _get_cfg(fcode):
             try:
                 line += blocks_id[output] + ","
             except:
-                # It fails because output is not in the function. It happens because of Radare.
-                # It can be ignored although it should be improved.
-                print(f'[!!] Failed to get callgraph. From node {offset} to node {output}')
+                pass
         line = (line[:-1] if len(outputs) > 0 else line) + ";"
 
     return line
 
 def _get_opcodes(fcode):
-    '''
-    Get opcodes from Radare assembly function.
-
-    Args:
-    - fcode = r2.cmdj("agj")
-
-    Returns:
-    - opcodes (str): Opcodes using str format
-    '''
-
     opcodes: dict[str, int] = {}
 
     for block in fcode[0]["blocks"]:
@@ -173,67 +127,49 @@ def _get_opcodes(fcode):
 
     return line
 
-def _features_to_hashes(features: dict[str, ], n_digits: int=2):
-    '''
-    Transform features/attributes to hashes.
-
-    Args:
-    - features (dict[str, ]): { Key: attribute name, Value: feature value }
-    - n_digits (int, optional): Number of digits for blake2b hash
-
-    Returns:
-    - hashes (dict[str, ]): { Key: attribute name, Value: feature hash }
-    '''
-
+def _features_to_hashes(features: dict[str, ]):
     fuzzy_hash: dict[str, ] = {}
     for feature, value in features.items():
         if feature in ('name', 'cfg', 'callgraph', 'opcodes', 'args', 'locals'):
-            fuzzy_hash[feature] = blake2b(str.encode(value), digest_size=n_digits).hexdigest()
+            fuzzy_hash[feature] = blake2b(str.encode(value), digest_size=2).hexdigest()
         elif feature == 'ninstrs':
             ninstrs = 0
-            # Quantiles
             limits = [*range(8, 40), *range(41, 80, 2), *range(81, 160, 3), *range(161, 320, 4), *range(321, 1000, 5)]
             for x in limits:
                 if value >= x: ninstrs += 1
             fuzzy_hash[feature] = ninstrs
         elif feature == 'indegree':
             indegree = 0
-            # Quantiles
             limits = [1, 2, 3, 4, 5, 6, 7, 8, 10, 13, 17, 22, 28, 35, 43]
             for x in limits:
                 if value >= x: indegree += 1
             fuzzy_hash[feature] = indegree
         elif feature == 'outdegree':
             outdegree = 0
-            # Quantiles
             limits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 16, 19, 23, 28]
             for x in limits:
                 if value >= x: outdegree += 1
             fuzzy_hash[feature] = outdegree
         elif feature == 'nvars':
             nlocals = 0
-            # Quantiles
             limits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 16, 19, 23, 28]
             for x in limits:
                 if value >= x: nlocals += 1
             fuzzy_hash[feature] = nlocals
         elif feature == 'cc':
             cc = 0
-            # Quantiles
             limits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 13, 16, 20, 25, 31]
             for x in limits:
                 if value >= x: cc += 1
             fuzzy_hash[feature] = cc
         elif feature == 'nblocks':
             nblocks = 0
-            # Quantiles
             limits = [1, 2, 3, 4, 5, 6, 7, 9, 12, 16, 21, 27, 34, 42, 51]
             for x in limits:
                 if value >= x: nblocks += 1
             fuzzy_hash[feature] = nblocks
         elif feature == 'stackframe':
             stackframe = 0
-            # Quantiles
             limits = [1, 4, 8, 12, 16, 20, 24, 28, 40, 56, 80, 120, 200, 360, 620]
             for x in limits:
                 if value >= x: stackframe += 1
@@ -254,8 +190,6 @@ def calculate_ccbhash(file: str):
 
     f_hash: dict[str, dict] = {}
     jsn = []
-    # If len(jsn) == 0 then Radare failed to open the file and we must repeat it
-    # Radare usually fails to open files for no reason but trying again works
     while len(jsn) == 0:
         r2 = r2pipe.open(file)
         r2.cmd("aaa")
@@ -266,32 +200,24 @@ def calculate_ccbhash(file: str):
         r2.cmd(f"s {function['offset']}")
         fcode = r2.cmdj("agj")
         if len(fcode) == 0:
-            # Error to get the code from current function
-            # We must continue getting the code from the rest of the functions
             print(f'[!!] Failed to get code from {function["name"]}')
             continue
         finf = r2.cmdj("afij")
         nargs = 0 if "nargs" not in function else function["nargs"]
         nlocals = 0 if "nlocals" not in function else function["nlocals"]
-        try:
-            variables = _get_variables(finf)
-            stackframe = function["stackframe"]
-            args = variables['args']
-            locals = variables['vars']
-            nvars = nargs + nlocals
-            indegree = function["indegree"]
-            outdegree = function["outdegree"]
-            ninstrs = function["ninstrs"]
-            nblocks = function["nbbs"]
-            cc = function["cc"]
-            opcodes = _get_opcodes(fcode)
-            cfg = _get_cfg(fcode)
-            callgraph = _get_callgraph(function['name'], global_callgraph)
-        except Exception as e:
-            print(f'[!!] Failed to get features/attributes from {function["name"]}')
-            print(e)
-            # We must continue calculating the ccbhash from the rest of the functions
-            continue
+        variables = _get_variables(finf)
+        stackframe = function["stackframe"]
+        args = variables['args']
+        locals = variables['vars']
+        nvars = nargs + nlocals
+        indegree = function["indegree"]
+        outdegree = function["outdegree"]
+        ninstrs = function["ninstrs"]
+        nblocks = function["nbbs"]
+        cc = function["cc"]
+        opcodes = _get_opcodes(fcode)
+        cfg = _get_cfg(fcode)
+        callgraph = _get_callgraph(function['name'], global_callgraph)
         f_attr = {'name': function['name'], 'nvars': nvars, 'args': args, 'locals': locals, 'opcodes': opcodes, 'nblocks': nblocks, 'cc': cc,
                   'indegree': indegree, 'outdegree': outdegree, 'ninstrs': ninstrs, 'cfg': cfg, 'callgraph': callgraph, 'stackframe': stackframe}
         f_hash[f_attr['name']] = _features_to_hashes(f_attr)
@@ -369,7 +295,6 @@ def compare_files(file1_hashes: dict[str, dict], file2_hashes: dict[str, dict]):
     '''
 
     scores: dict[str, tuple[float, str]] = {}
-    similarity: float = 0
 
     for func1, hash1 in file1_hashes.items():
         max_score, max_func = 0, ''
@@ -381,16 +306,8 @@ def compare_files(file1_hashes: dict[str, dict], file2_hashes: dict[str, dict]):
                 max_score, max_func = score, func2
         scores[func1] = max_score, max_func
 
-    try:
-        if len(scores.items()) > 0:
-            scores = dict(sorted(scores.items(), key=lambda item: item[1][0], reverse=True))
-            similarity = mean([x[0] for x in scores.values()])
-        else:
-            scores = None
-            similarity = None
-    except StatisticsError:
-        scores = None
-        similarity = None
+    scores = dict(sorted(scores.items(), key=lambda item: item[1][0], reverse=True))
+    similarity: float = mean([x[0] for x in scores.values()])
 
     return scores, similarity
 
@@ -458,14 +375,12 @@ def update_db_from_samples(main_dir: str, output_file: str = 'bbdd.json'):
     '''
 
     dirs = os.listdir(main_dir)
-    # .DS_Store is ignored if it exists
     try: dirs.remove('.DS_Store')
     except: pass
 
     files: dict[str, list[str]] = {}
     for dir in dirs:
         files[dir] = os.listdir(f'{main_dir}/{dir}')
-        # .DS_Store is ignored if it exists
         try: files[dir].remove('.DS_Store')
         except: pass
 
@@ -488,12 +403,10 @@ def get_graph(func: str, dir: str) -> tuple[str, str]:
     - dir (str): Directory with the sample
 
     Returns:
-    - CFG, callgraph (tuple[str, str]): Function CFG using ASCII art. If function does not exist, None is returned
+    - CFG, callgraph (tuple[str, str]): Function CFG using ASCII art
     '''
 
     jsn = []
-    # If len(jsn) == 0 then Radare failed to open the file and we must repeat it
-    # Radare usually fails to open files for no reason but trying again works
     while len(jsn) == 0:
         r2 = r2pipe.open(dir)
         r2.cmd("aaa")
@@ -502,7 +415,4 @@ def get_graph(func: str, dir: str) -> tuple[str, str]:
         if f['name'] == func:
             r2.cmd(f"s {f['offset']}")
             return r2.cmd("agf"), r2.cmd("agc")
-    # If the function does not exist, None is returned
-    # An exception could be raised instead of returning None
-    # return None
-    raise Exception(f'Error to get graphs from function {func}. The function may not exist')
+    return None
